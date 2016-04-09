9ca6bf387b7c4f101a4744d6bdbd905758063a14
jjluke <jj.luke808@gmail.com>
2016-04-08 17:38:03 -0700

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,14 @@
 from flask import Flask
 import os
-port = os.environ.get('PORT')
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello!"
+    return "Hello World!"
 
 if __name__ == "__main__":
-    app.run("0.0.0.0",port)
+    port = os.environ.get("PORT")
+    app.run(
+        "0.0.0.0"
+    , port    
+    )
